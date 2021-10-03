b9caee5d01252e5b2d59d25f0f0ea1ea3da3b63e
Marisol Bennett <marisol_ramirez21@live.com>
2021-10-03 01:12:42 -0500

    added other files and folders and cleaned up code

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,6 @@
-
+// generatePage function
+const generatePage = (name, github) => {
+    return `
     <!DOCTYPE html> 
     <html lang="en"> 
     <head>
@@ -9,8 +11,11 @@
     </head>
   
     <body>
-      <h1>Marisol</h1>
-      <h2><a href="https://github.com/marisolgithub">Github</a></h2>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-    
+    `;
+  };
+
+  module.exports = generatePage;
