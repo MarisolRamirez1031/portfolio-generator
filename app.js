7d88b877a70650499275c8d975afdb6a7a04fae3
Marisol Bennett <marisol_ramirez21@live.com>
2021-10-03 00:47:26 -0500

    created HTML file via node.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
 
+const fs = require('fs');
+
 // generatePage function
 const generatePage = (userName, githubName) => {
   return `
@@ -22,8 +24,12 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
 
 /*const printProfileData = profileDataArr => {
   // This...
