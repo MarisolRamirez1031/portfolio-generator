d4d7a03aa9addad59a5cc80f95259ad01aa51a2a
Marisol Bennett <marisol_ramirez21@live.com>
2021-10-03 01:15:03 -0500

    more changes

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const profileDataArgs = process.argv.slice(2);
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-=======
 const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
@@ -29,4 +12,3 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
->>>>>>> feature/generate-webpage
